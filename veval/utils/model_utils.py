4ad1d84a44a72e34fb7feba34297d1cd1cf8da03
xeon27 <omkar.dige@vectorinstitute.ai>
2024-05-02 11:30:43 -0400

    Implement rate limiter

--- veval/utils/model_utils.py
+++ veval/utils/model_utils.py
@@ -1,6 +1,8 @@
 import cohere
+import time
 
-from typing import Any, Dict, Optional
+from functools import wraps
+from typing import Any, Dict, Optional, Callable
 
 from llama_index.core.llms import (
    CustomLLM,
@@ -17,6 +19,8 @@ DEFAULT_MODEL = "openai-gpt-3.5-turbo"
 DEFAULT_CONTEXT_WINDOW = 16385 # TODO: Verify this
 DEFAULT_TEMPERATURE = 0.0
 DEFAULT_SEED = 37
+DEFAULT_RL_LIMIT = 50
+DEFAULT_RL_INTERVAL = 60.0
 
 LM_MODEL_CONFIG = {
    "gpt-3.5-turbo": {
@@ -29,6 +33,31 @@ LM_MODEL_CONFIG = {
       "context_window": 4096,
    },
    }
+
+
+def rate_limiter(limit: int, interval: float) -> Callable:
+   def decorator(func: Callable) -> Callable:
+      calls = 0
+      last_reset = time.time()
+
+      @wraps(func)
+      def wrapper(*args, **kwargs):
+         nonlocal calls, last_reset
+         calls += 1
+         if calls > limit:
+            elapsed_time = time.time() - last_reset
+            if elapsed_time < interval:
+               time.sleep(interval - elapsed_time)
+               last_reset = time.time()
+               calls = 1
+            else:
+               last_reset = time.time()
+               calls = 1
+         return func(*args, **kwargs)
+
+      return wrapper
+
+   return decorator
 
 
 def trim_predictions_to_max_token_length(tokenizer, prediction, max_token_length=128):
@@ -101,6 +130,10 @@ class LlamaIndexLLM(CustomLLM):
             is_chat_model=self.is_chat_model,
          )
    
+   @rate_limiter(
+      limit=DEFAULT_RL_LIMIT, 
+      interval=DEFAULT_RL_INTERVAL,
+   )
    @llm_completion_callback()
    def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
       
